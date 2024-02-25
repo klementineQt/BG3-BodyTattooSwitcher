ba419f24817d6dbe0beca8dd08c3c7994c3178eb
Klementine Des Pêches <klementineQt@proton.me>
2024-02-24 23:12:00 -0500

    ok now functional??

--- Mods/kQt_BodyTattooSwitcher/ScriptExtender/Lua/Shared/Utils.lua
+++ Mods/kQt_BodyTattooSwitcher/ScriptExtender/Lua/Shared/Utils.lua
@@ -1,5 +1,5 @@
 function Utils.AddBTSSpell(target)
-    if Osi.HasSpell(target, Constants.SpellContainer) == - then
+    if Osi.HasSpell(target, Constants.SpellContainer) == 0 then
         Osi.AddSpell(target, Constants.SpellContainer, 1, 1)
     end
 end
